940b2fcc1f0e5261ea223193c1a15a247aa0fdf8
Shaked Shalom <shaked201098@email.com>
2023-05-31 18:03:28 +0300

    add_trainer

--- users/Trainer.py
+++ users/Trainer.py
@@ -1,15 +1,31 @@
+import datetime
+
 class Trainer:
-    def __init__(self, trainerID, trainerFullName, specialty, hireDate, bankAccount, email, phoneNumber, loginDetails, hourlyWage, managerID):
+    def __init__(self, connector, trainerID, trainerFullName, specialty, bankAccount, email, phoneNumber, loginDetails, hourlyWage, managerID):
         self.trainerID = trainerID
         self.trainerFullName = trainerFullName
         self.specialty = specialty
-        self.hireDate = hireDate
+        self.hireDate = datetime.date.today()
         self.bankAccount = bankAccount
         self.email = email
         self.phoneNumber = phoneNumber
         self.loginDetails = loginDetails
         self.hourlyWage = hourlyWage
         self.managerID = managerID
+        self.connector= connector
+        sql= "INSERT INTO Trainer  (trainerID, trainerFullName, specialty, hireDate, bankAccount, email, phoneNumber, loginDetails, hourlyWage, managerID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        values = (self.trainerID, self.trainerFullName, self.specialty, str(self.hireDate), self.bankAccount, self.email, self.phoneNumber , self.loginDetails, self.hourlyWage, self.managerID )
+        self.connector.execute_query(sql, values)
+
+    def set_trainerID(self, trainerID):
+        sql = "UPDATE Trainer SET trainerID=? WHERE trainerID=?;"
+        values = (trainerID, self.trainerID)
+        self.connector.execute_query(sql, values)
+        self.trainerID = trainerID
+
+    def delete(self):
+        sql = "DELETE FROM Trainee WHERE traineeID=?;"
+        self.connector.execute_query(sql, self.traineeID)
 
     def shiftAssignment(self, Training):
         pass
