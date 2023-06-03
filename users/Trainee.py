39423a22530352c131fcf6cc048a39ad81a8f2d1
Shir Elyakim <shirelyakim2@gmail.com>
2023-06-03 14:51:17 +0300

    done

--- users/Trainee.py
+++ users/Trainee.py
@@ -1,16 +1,17 @@
 from app import db
+import datetime
 
-class Trainee:
+class Trainee(db.Model):
     traineeID = db.Column(db.Integer, primary_key=True)
     traineeFullName = db.Column(db.String)
     bankAccount = db.Column(db.String)
     joiningDate = db.Column(db.Date)
     birthday = db.Column(db.Date)
-    email = db.Column(db.Integer, primary_key=True)
-    phoneNumber = db.Column(db.Integer, primary_key=True)
-    membershipID = db.Column(db.Integer, primary_key=True)
-    loginDetails = db.Column(db.Integer, primary_key=True)
-    recruitedBy = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    phoneNumber = db.Column(db.String)
+    membershipID = db.Column(db.Integer)
+    loginDetails = db.Column(db.String)
+    recruitedBy = db.Column(db.String)
 
     def __init__(self, traineeID, traineeFullName, bankAccount, birthday, email, phoneNumber, membershipID, loginDetails, recruitedBy):
         self.traineeID = traineeID
