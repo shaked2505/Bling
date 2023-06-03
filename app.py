208a8e47fea401a053ad826adf62773f8d806aae
Shir Elyakim <shirelyakim2@gmail.com>
2023-06-03 13:53:48 +0300

    add connector

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import socket
 import urllib
 
 application = Flask(__name__)
-params = urllib.parse.quote_plus('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-GRD4TBL;DATABASE=bling;Trusted_Connection=yes;')
+params = urllib.parse.quote_plus(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={socket.gethostname()};DATABASE=bling;Trusted_Connection=yes;')
 application.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 application.config['SQLALCHEMY_ECHO'] = True
--- users/Trainee.py
+++ users/Trainee.py
@@ -1,7 +1,18 @@
-import datetime
+from app import db
 
 class Trainee:
-    def __init__(self, connector, traineeID, traineeFullName, bankAccount, birthday, email, phoneNumber, membershipID, loginDetails, recruitedBy):
+    traineeID = db.Column(db.Integer, primary_key=True)
+    traineeFullName = db.Column(db.String)
+    bankAccount = db.Column(db.String)
+    joiningDate = db.Column(db.Date)
+    birthday = db.Column(db.Date)
+    email = db.Column(db.Integer, primary_key=True)
+    phoneNumber = db.Column(db.Integer, primary_key=True)
+    membershipID = db.Column(db.Integer, primary_key=True)
+    loginDetails = db.Column(db.Integer, primary_key=True)
+    recruitedBy = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, traineeID, traineeFullName, bankAccount, birthday, email, phoneNumber, membershipID, loginDetails, recruitedBy):
         self.traineeID = traineeID
         self.traineeFullName = traineeFullName
         self.bankAccount = bankAccount
@@ -12,33 +23,3 @@ class Trainee:
         self.membershipID = membershipID
         self.loginDetails = loginDetails
         self.recruitedBy = recruitedBy
-        self.connector = connector
-        sql= "INSERT INTO Trainee (traineeID, traineeFullName, bankAccount, birthday, email, phoneNumber, joiningDate, membershipID , loginDetails, recruitedBy ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        values = (self.traineeID, self.traineeFullName, self.bankAccount, str(self.birthday), self.email, self.phoneNumber, str(self.joiningDate), self.membershipID , self.loginDetails, self.recruitedBy)
-        self.connector.execute_query(sql, values)
-
-    def set_traineeID(self, traineeID):
-        sql = "UPDATE Trainee SET traineeID=? WHERE traineeID=?;"
-        values = (traineeID, self.traineeID)
-        self.connector.execute_query(sql, values)
-        self.traineeID = traineeID
-
-    def delete(self):
-        sql = "DELETE FROM Trainee WHERE traineeID=?;"
-        self.connector.execute_query(sql, self.traineeID)
-
-    def registerationReq(self, TrainingRegistrationForm):
-        pass
-    def getPersonalProfileData(self, loginDetails):
-        pass
-    def setPersonalProfileData(self, loginDetails):
-        pass
-    def getMembershipPlan(self, membershipID):
-        pass
-    def setMembershipPlan(self, membershipID):
-        pass
-    def membershipCancellationRequestForm(self, MembershipCancellationRequestForm):
-        pass
-    def trainingCancellationRequestForm(self, TrainingCancellationRequestForm):
-        pass
-
