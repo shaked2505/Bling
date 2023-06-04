ebdf3a1f2c34b427381f0c1d0af7ec280e9e8aab
Shir Elyakim <shirelyakim2@gmail.com>
2023-06-04 16:10:26 +0300

    Done_MemCanReqForm

--- forms/MembershipCancellationRequestForm.py
+++ forms/MembershipCancellationRequestForm.py
@@ -1,34 +1,36 @@
+from app import db
 import datetime
 
-class MembershipCancellationRequestForm:
-    def __init__(self, connector, requestID, memberID,planID,reason,approvalStatus):
+class MembershipCancellationRequestForm(db.Model):
+    requestID = db.Column(db.Integer, primary_key=True)
+    traineeID = db.Column(db.Integer, db.ForeignKey('trainee.traineeID'))
+    trainee = db.relationship('Trainee', backref=db.backref('Trainee', uselist=False))
+    membershipID = db.Column(db.Integer, db.ForeignKey('membership_plan.membershipID'))
+    Membership_plan = db.relationship('MembershipPlan', backref=db.backref('Membership_plan', uselist=False))
+    reason = db.Column(db.String)
+    requestDate = db.Column(db.DateTime, default=datetime.now)
+    approvalStatus = db.Column(db.String)
+
+    # Create initializer/constructor
+    def __init__(self, requestID, traineeID, membershipID, reason, approvalStatus):
         self.requestID = requestID
-        self.memberID = memberID
-        self.planID = planID
+        self.traineeID = traineeID
+        self.membershipID = membershipID
         self.reason = reason
         self.requestDate = datetime.date.today()
         self.approvalStatus = approvalStatus
-        self.connector = connector
 
 
-        sql= "INSERT INTO MembershipCancellationRequestForm (requestID, memberID, planID, reason, requestDate,approvalStatus ) VALUES (?, ?, ?, ?, ?, ?)"
-        values = (self.requestID, self.memberID, self.planID,self.reason ,str(self.requestDate), self.approvalStatus)
-        self.connector.execute_query(sql, values)
 
-    def set_requestID(self, requestID):
-        sql = "UPDATE MembershipCancellationRequestForm SET requestID=? WHERE requestID=?;"
-        values = (requestID, self.requestID)
-        self.connector.execute_query(sql, values)
-        self.requestID = requestID
+    def getRequestDetails(self):
+        return self.requestID
+        
 
-    def delete(self):
-        sql = "DELETE FROM MembershipCancellationRequestForm WHERE requestID=?;"
-        self.connector.execute_query(sql, self.requestID)
+    def isPending(self):
+        return self.approvalStatus
 
 
-    def getRequestDetails(self, requestID):
-        pass
-    def isPending(self, requestID):
-        pass
     def notificationEmailToSystemManager(self, systemManager):
         pass
+
+
