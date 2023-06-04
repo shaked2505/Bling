2215026b29aca9f742c6e79d317093dcba0ad50c
Shir Elyakim <shirelyakim2@gmail.com>
2023-06-04 22:43:43 +0300

    add schedule table

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ from main_services.SpecificTimeTraining import SpecificTimeTraining
 
 @application.route("/")
 def landing():
-    #db.session.add(Trainer(66, 'Miri Bar Lev', 'Power and MatPilates', str(date(2022, 7, 18)), '12-294-972547', 'miri@gmail.com', '052-8653908', 'MiriBarLev66', '65.00', 1111))
-    #db.session.add(BrandedMerchandise(300, 50.00, 20, 'Water Bottle', 'Stainless steel bottle, contains 500 ml', 1111, None))
-    #db.session.add(SystemManager(1111, 'Bar Diamant',  '10-350-789543', 'bar154@gmail.com', '054-7895279', 'Bar Diamant1111'))
-    db.session.commit()
+    # db.session.add(Trainer(66, 'Miri Bar Lev', 'Power and MatPilates', str(date(2022, 7, 18)), '12-294-972547', 'miri@gmail.com', '052-8653908', 'MiriBarLev66', '65.00', 1111))
+    # db.session.add(BrandedMerchandise(300, 50.00, 20, 'Water Bottle', 'Stainless steel bottle, contains 500 ml', 1111, None))
+    # db.session.add(SystemManager(1111, 'Bar Diamant',  '10-350-789543', 'bar154@gmail.com', '054-7895279', 'Bar Diamant1111'))
+    # db.session.commit()
     return render_template("test.html")
+
+@application.route("/schedule")
+def schedule():
+    return render_template("schedule.html")
 
 if __name__ == '__main__':
     with application.app_context():
