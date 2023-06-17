487a2419cf4fb67ebc925e6b784f7d90ac1dba98
Shaked Shalom <shaked201098@email.com>
2023-06-17 16:43:43 +0300

    fix all time

--- forms/TrainingRegistrationForm.py
+++ forms/TrainingRegistrationForm.py
@@ -2,8 +2,6 @@ import datetime
 from sqlalchemy import ForeignKeyConstraint
 from main_services.SpecificTimeTraining import SpecificTimeTraining
 from app import db
-current_time = datetime.datetime.now()
-timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 class TrainingRegistrationForm(db.Model):
     registrationID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -19,14 +17,13 @@ class TrainingRegistrationForm(db.Model):
     approvalStatus = db.Column(db.String)
 
     # Create initializer/constructor
-    def __init__(self, traineeID, trainingID, specificTimeTrainingDate, approvalStatus, requestDate=None, requestTime=None):
+    def __init__(self, traineeID,trainingID, approvalStatus, specificTimeTrainingDate, requestDate=None ,requestTime=None):
         self.traineeID = traineeID
         self.trainingID = trainingID
         self.specificTimeTrainingDate = specificTimeTrainingDate
         self.approvalStatus = approvalStatus
         self.requestDate = requestDate or datetime.date.today()
         self.requestTime = requestTime or datetime.datetime.now().time()
-
 
 # class TrainingRegistrationForm:
 #     def __init__(self, connector,registrationID, memberID, trainingID, specificTimeTrainingDate ,approvalStatus):
